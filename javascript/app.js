327cbc0432c3ab7de464fd81c1e43620bdc8fcd9
Mohamed Elsayed <96896650+MohamedElsayed2202@users.noreply.github.com>
2022-09-06 18:33:03 +0200

    Added Database (#2)

--- javascript/app.js
+++ javascript/app.js
@@ -1,28 +1,23 @@
-// import {initializeApp} from 'firebase/app';
-// import {firebaseConfig} from '../config';
-// import {Auth} from './auth'
-// // const config = require('../config');
-// const app = initializeApp(firebaseConfig);
+// 'https://www.gstatic.com/firebasejs/9.9.4/firebase'
+import {initializeApp} from 'https://www.gstatic.com/firebasejs/9.9.4/firebase-app.js';
+import {Auth} from './auth.js'
+import { DB } from './db.js';
+const firebaseConfig = {
+  apiKey: "AIzaSyBTNtE77ZFyWiB8F8low9IpAr7hT-mBqvI",
+  authDomain: "udemy-clone-69601.firebaseapp.com",
+  projectId: "udemy-clone-69601",
+  storageBucket: "udemy-clone-69601.appspot.com",
+  messagingSenderId: "1032707806231",
+  appId: "1:1032707806231:web:c76c757ff5910e9e6bf852"
+};
+const app = initializeApp(firebaseConfig);
+const db = new DB(app);
+
+db.createUser('123456789','Mohamed Elsayed','Student');
 // const auth = new Auth(app);
 // auth.SignUp();
-import { initializeApp } from "firebase/app";
-import { getAuth, createUserWithEmailAndPassword } from "@firebase/auth";
-const firebaseConfig = {
-    apiKey: "AIzaSyBTNtE77ZFyWiB8F8low9IpAr7hT-mBqvI",
-    authDomain: "udemy-clone-69601.firebaseapp.com",
-    projectId: "udemy-clone-69601",
-    storageBucket: "udemy-clone-69601.appspot.com",
-    messagingSenderId: "1032707806231",
-    appId: "1:1032707806231:web:c76c757ff5910e9e6bf852"
-  };
-  const app = initializeApp(firebaseConfig);
 
-  const auth = getAuth(app);
 
-  createUserWithEmailAndPassword(auth, 'beadoo@gmail.com', 123456)
-  .then(createUser => {
-    console.log(createUser.user.uid);
-  })
-  .catch(e => console.log(e))
+
 
 
---
+++ javascript/modals/user.js
@@ -0,0 +1,7 @@
+export class User{
+    constructor(id, name, type){
+        this.id = id;
+        this.name = name;
+        this.type = type;
+    }
+};
