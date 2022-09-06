cf56f49b9dbc7604ad52197a012ed86d48d6ad26
MohamedElsayed2202 <mohamed.elsayed.1.3.1999@gmail.com>
2022-09-07 00:51:06 +0200

    Strated using firbase Storage

--- javascript/auth.js
+++ javascript/auth.js
@@ -1,4 +1,4 @@
-// 'https://www.gstatic.com/firebasejs/9.9.4/firebase-auth.js'
+// 'https://www.gstatic.com/firebasejs/9.9.4/firebase-auth.js' firebase/auth
 import {getAuth, createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut} from 'https://www.gstatic.com/firebasejs/9.9.4/firebase-auth.js';
 import { DB } from './db.js';
 export class Auth {
@@ -7,19 +7,60 @@ export class Auth {
         this.db = new DB(app);
     }
 
-    async signUp (email, password, name, type) {
+    async signUp (email, password, name, type, image) {
         try{
             let user = await createUserWithEmailAndPassword(this.auth, email, password);
             let uid = user.user.uid;
-            console.log(uid);
             let token = await user.user.getIdToken();
             localStorage.setItem('uid', uid);
             localStorage.setItem('token', token);
-            this.db.createUser(uid, name, type);
+            localStorage.setItem('type', type);
+            this.db.createUser(uid, name, type, image);
         }catch(e){
-            console.log(e)
+            console.log(e.code);
+            console.log(e.message);
         }
 
+    }
+    async signIn (email, password, type){
+        
+       try{
+        let user = await signInWithEmailAndPassword(this.auth, email, password);
+        let uid = user.user.uid;
+        let token = await user.user.getIdToken();
+        localStorage.setItem('uid', uid);
+        localStorage.setItem('token', token);
+        localStorage.setItem('type', type);
+       }catch(e){
+        console.log(e.code);
+        console.log(e.message);
+       }
+    }
+
+    async signOut(){
+        try{
+            await signOut(this.auth)
+        }catch(e){
+            console.log(e.code);
+            console.log(e.message);
+        }
+    }
+
+    checkUserStatus(){
+        onAuthStateChanged(this.auth, (user) => {
+            if(user){
+                this.db.getUser(user.uid)
+                .then(user => {
+                    
+                });
+                // console.log(userData)
+                // if(userData.type =='student'){
+                    
+                // }
+            }else{
+                
+            }
+        })
     }
 };
 
